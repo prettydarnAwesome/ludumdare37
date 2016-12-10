a795465b5ece196679e1969daa7850361cebbf63
zonalhaz <huw@talliss.com>
2016-12-10 23:18:00 +0000

    Enabled physics on box and let you grab box with both hands

--- Assets/WandGrabber.cs
+++ Assets/WandGrabber.cs
@@ -1,48 +1,64 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class WandGrabber : MonoBehaviour {
+public class WandGrabber : MonoBehaviour
+{
+    private SteamVR_TrackedController trackedController;
+    private Queue<Tuple<Vector3, float>>previousPositions;
 
-    SteamVR_TrackedController trackedController;
-
-	// Use this for initialization
-	void Start () {
-
-        trackedController = GetComponent<SteamVR_TrackedController>();
-        if (trackedController == null)
-        {
-            trackedController = gameObject.AddComponent<SteamVR_TrackedController>();
-        }
-
-        trackedController.TriggerClicked += TrackedController_TriggerClicked;
-
-    }
-
-    private void TrackedController_TriggerClicked(object sender, ClickedEventArgs e)
+    // Use this for initialization
+    void Start()
     {
-        throw new System.NotImplementedException();
+        trackedController = GetComponentInParent<SteamVR_TrackedController>();
+        previousPositions = new Queue<Tuple<Vector3, float>>();
     }
 
     // Update is called once per frame
-    void Update () {
-		
-	}
-
-    void OnTriggerStay(Collider other)
-    {
-        if (trackedController.triggerPressed)
+    void FixedUpdate()
+    {               
+        if (transform.childCount != 0)
         {
-            if (transform.childCount != 0)
+            if (trackedController.triggerPressed)
             {
-                other.gameObject.transform.parent = transform;
+                if (previousPositions.Count >= 5)
+                {
+                    previousPositions.Dequeue();
+                }
+                previousPositions.Enqueue(new Tuple<Vector3,float>( transform.position,Time.deltaTime));
             }
             else
             {
+                transform.GetChild(0).GetComponent<Rigidbody>().isKinematic = false;
+                List<Vector3> velocities = new List<Vector3>();
+                for (var i = 0; i < previousPositions.Count - 1; i++)
+                {
+                    velocities.Add((previousPositions.ElementAt(i + 1).Item1 - previousPositions.ElementAt(i).Item1)/previousPositions.ElementAt(i+1).Item2);
+                }
+                var dx = velocities.Average(v => v.x);
+                var dy = velocities.Average(v => v.y);
+                var dz = velocities.Average(v => v.z);
+
+                Debug.Log("Given Velocity:" + dx + "," + dy + "," + dz);
+                
+                transform.GetChild(0).GetComponent<Rigidbody>().AddForce(dx,dy,dz, ForceMode.Impulse);
+
                 transform.DetachChildren();
-            }        
-        }        
+            }
+        }
     }
 
-    
+    void OnTriggerStay(Collider other)
+    {
+        if (trackedController.triggerPressed && transform.childCount == 0)
+        {
+            other.gameObject.transform.parent = transform;
+            other.GetComponent<Rigidbody>().isKinematic = true;
+        }
+    }
+
+
+
 }
