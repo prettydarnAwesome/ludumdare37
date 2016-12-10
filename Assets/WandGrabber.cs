6779ed5e0f7b38a84dfb3174d05c20fe586aeacf
zonalhaz <huw@talliss.com>
2016-12-10 21:34:19 +0000

    Started adding a grabber

---
+++ Assets/WandGrabber.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WandGrabber : MonoBehaviour {
+
+    SteamVR_TrackedController trackedController;
+
+	// Use this for initialization
+	void Start () {
+
+        trackedController = GetComponent<SteamVR_TrackedController>();
+        if (trackedController == null)
+        {
+            trackedController = gameObject.AddComponent<SteamVR_TrackedController>();
+        }
+
+        trackedController.TriggerClicked += TrackedController_TriggerClicked;
+
+    }
+
+    private void TrackedController_TriggerClicked(object sender, ClickedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    void OnTriggerStay(Collider other)
+    {
+        if (trackedController.triggerPressed)
+        {
+            if (transform.childCount != 0)
+            {
+                other.gameObject.transform.parent = transform;
+            }
+            else
+            {
+                transform.DetachChildren();
+            }        
+        }        
+    }
+
+    
+}
